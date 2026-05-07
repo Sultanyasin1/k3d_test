df15ae6844934a6d48e301c12492e05da9305685
Sultanyasin1 <sultan.yasin@ekerit.se>
2026-05-07 15:02:24 +0200

    UI

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function TestRoute() {
-  return <div>TestRoute</div>;
+  return <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+    <p>TestRoute</p>
+    <h1 style={{ color: "blue" }}>Test Route</h1>
+    <h2>Test Route</h2>
+  </div>;
 }
 
 export default TestRoute;
