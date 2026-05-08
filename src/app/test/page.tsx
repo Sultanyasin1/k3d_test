3db312a2bf0187bfeb4b58854874884018e2e436
Sultanyasin1 <sultan.yasin@ekerit.se>
2026-05-08 10:25:44 +0200

    style

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,7 +4,7 @@ function TestRoute() {
   return <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
     <p>TestRoute</p>
     <h1 style={{ color: "blue", fontSize: "2rem" }}>Test Route</h1>
-    <h1 style={{ color: "red",  }}>Test Route</h1>
+    <h1 style={{ color: "red", fontSize: "4rem" }}>Test Route</h1>
     <h2>Test Route</h2>
   </div>;
 }
