0ed608365ed1c389d41424d170a44877ed7c5162
Sultanyasin1 <sultan.yasin@ekerit.se>
2026-05-19 16:48:12 +0200

    newRoute for test

---
+++ src/app/test2/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Test2() {
+  return (
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center"}} >Test2</div>
+  )
+}
+
+export default Test2
